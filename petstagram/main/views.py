c02275c2619ebedd55d08fb452fd8282fe7d5e9d
TanyaAng <t.j.angelova@gmail.com>
2022-07-03 14:02:05 +0300

    Web App

--- petstagram/main/views.py
+++ petstagram/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from petstagram.main.models import Profile, PetPhoto
+from petstagram.main.models import Profile, PetPhoto, Pet
 
 
 def get_profile():
@@ -19,7 +19,7 @@ def show_home(request):
 
 def show_dash_board(request):
     profile = get_profile()
-    pet_photos = set(PetPhoto.objects.prefetch_related('tagged_pets').filter(tagged_pets__user_profile=profile))
+    pet_photos = PetPhoto.objects.prefetch_related('tagged_pets').filter(tagged_pets__user_profile=profile).distinct()
     context = {
         'pet_photos': pet_photos,
     }
@@ -28,8 +28,16 @@ def show_dash_board(request):
 
 def show_profile(request):
     profile = get_profile()
+    pets = Pet.objects.filter(user_profile=profile)
+
+    pet_photos = PetPhoto.objects.filter(tagged_pets__in=pets).distinct()
+    total_pet_photos_count = len(pet_photos)
+    total_likes_count = sum(pp.likes for pp in pet_photos)
+
     context = {
         'profile': profile,
+        'total_likes_count': total_likes_count,
+        'total_pet_photos_count': total_pet_photos_count,
     }
     return render(request, 'profile_details.html', context)
 
